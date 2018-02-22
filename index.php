80eb694812b486083462d9f6315714ab484779b9
Vitris <tug24657@temple.edu>
2018-02-22 12:11:36 -0500

    added home page, nothing working yet

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php include './common/config.php' ?>
+<?php include './view/homeHeader.php' ?>
+
+<div class="container-fluid d-flex justify-content-center splashSection">
+  <div class="row">
+    <div class="col-lg-10 align-self-center text-center mx-auto">
+      <h1 style="font-weight: 800; color: yellow;">Improving Software License Management</h1>
+      <a role="button" class="btn btn-secondary btn-lg mt-4" href="#about">
+        Learn More
+      </a>
+    </div>
+  </div>
+</div>
+
+<div id="about" class="container my-5">
+  <div class="row">
+    <div class="col-md-7">
+      <h2>About Marka</h2>
+      <p class="lead">Marka is a mobile application that eliminates the inconvenience of cash-based, in-person transactions that are currently needed to sell beach access tags in the state of New Jersey.</p>
+      <p>The Marka app can be downloaded onto mobile phones, providing beachgoers with an opportunity to purchase digital beach passes prior to their visit to participating beaches.</p>
+    </div>
+    <div class="col-md-5">
+      <img class="img-fluid mx-auto" src="./img/nj-tag-locations.png" alt="Marka tags locations">
+    </div>
+  </div>
+</div>
+
+<?php include './view/homeFooter.php' ?>
