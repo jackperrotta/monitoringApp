a0a1ea7932e230d0d8ec7baf73cd1bc7001efbe3
Vitris <tug24657@temple.edu>
2018-02-26 22:34:53 -0500

    nothing really end of night

---
+++ registration/login.php
@@ -0,0 +1,11 @@
+<?php include '../view/homeHeader.php' ?>
+
+<div class="container">
+  <div class="row mx-auto">
+    <div class="col-lg-10 mx-auto">
+
+    </div>
+  </div>
+</div>
+
+<?php include '../view/homeFooter.php' ?>
