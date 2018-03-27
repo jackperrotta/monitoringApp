f66c3314ea69f96c7d8149b1f2cc71497935339d
Vitris <tug24657@temple.edu>
2018-03-27 15:07:55 -0400

    switch to prod config

--- common/config.php
+++ common/config.php
@@ -1,17 +1,17 @@
 <?php
     //this is for the database
-    $dsn = 'mysql:host=localhost;dbname=monitor';
-    $username = 'monitor';
+    // $dsn = 'mysql:host=localhost;dbname=monitor';
+    // $username = 'monitor';
+    // $password = 'password';
+
+    //this path can be used in place of relative references in HTML
+    // $base_path = 'http://localhost/monitoringApp';
+
+    //this is for the database
+    $dsn = 'mysql:host=monitoringapp.c6ufur3cegqo.us-west-2.rds.amazonaws.com:3306;dbname=monitoringApp';
+    $username = 'admin';
     $password = 'password';
 
     //this path can be used in place of relative references in HTML
-    $base_path = 'http://localhost/monitoringApp';
-
-    //this is for the database
-    // $dsn = 'mysql:host=localhost;dbname=db_tug24657';
-    // $username = 'tug24657';
-    // $password = '2JvA6ZVA';
-
-    //this path can be used in place of relative references in HTML
-    // $base_path = 'http://misdemo.temple.edu/tug24657/monitoringApp';
+    $base_path = 'http://54.244.209.143';
 ?>
