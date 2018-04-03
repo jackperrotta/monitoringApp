12f7594eaec827a93c2c863bfc75a0fc712a6089
Vitris <tug24657@temple.edu>
2018-04-03 15:08:11 -0400

    switch to prod basepath

--- common/config.php
+++ common/config.php
@@ -5,7 +5,7 @@
     // $password = 'password';
 
     //this path can be used in place of relative references in HTML
-    $base_path = 'http://localhost/monitoringApp';
+    // $base_path = 'http://localhost/monitoringApp';
 
     //this is for the database
     $dsn = 'mysql:host=monitoringapp.c6ufur3cegqo.us-west-2.rds.amazonaws.com:3306;dbname=monitoringApp';
@@ -13,5 +13,5 @@
     $password = 'password';
 
     //this path can be used in place of relative references in HTML
-    // $base_path = 'http://34.210.71.221';
+    $base_path = 'http://34.210.71.221';
 ?>
