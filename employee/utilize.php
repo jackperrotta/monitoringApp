4bf107a71f37aaf771c99528c8a7d88806148410
Vitris <tug24657@temple.edu>
2018-04-24 14:16:40 -0400

    new export link

--- employee/utilize.php
+++ employee/utilize.php
@@ -84,7 +84,9 @@
          <div class="btn-toolbar mb-2 mb-md-0">
            <div class="btn-group mr-2">
              <button class="btn btn-sm btn-outline-secondary">Share</button>
-             <button class="btn btn-sm btn-outline-secondary">Export</button>
+             <a href="../img/Salesforce.csv" download>
+               <button class="btn btn-sm btn-outline-secondary">Export</button>
+             </a>
            </div>
            <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
              <span data-feather="calendar"></span>
@@ -136,7 +138,7 @@
            </tbody>
          </table>
 
-           <h5><a href="../img/Salesforce.csv" download>Export CSV</a></h5>
+           <!-- <h5><a href="../img/Salesforce.csv" download>Export CSV</a></h5> -->
      </main>
    </div>
  </div>
