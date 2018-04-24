21a51d0c83359d06d7838fdf29de38b75c4d2856
Vitris <tug24657@temple.edu>
2018-04-24 14:17:43 -0400

    this shit annoying

--- employee/utilize.php
+++ employee/utilize.php
@@ -84,9 +84,13 @@
          <div class="btn-toolbar mb-2 mb-md-0">
            <div class="btn-group mr-2">
              <button class="btn btn-sm btn-outline-secondary">Share</button>
-             <a href="../img/Salesforce.csv" download>
-               <button class="btn btn-sm btn-outline-secondary">Export</button>
-             </a>
+
+               <button class="btn btn-sm btn-outline-secondary">
+                 <a href="../img/Salesforce.csv" download>
+                   Export
+                </a>
+                 </button>
+
            </div>
            <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
              <span data-feather="calendar"></span>
